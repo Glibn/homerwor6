5c59d3c75ca9b129d981cd2cb33fca96460747d1
Glibn <romanuting@gmail.com>
2025-05-16 23:55:45 +0300

    d

--- hom.js
+++ hom.js
@@ -1,1 +1,30 @@
-while (condition) {}
+// let i = 0;
+// while (i <= 10) {
+//   console.log(i);
+//   i += 1;
+// }
+// for (let i = 0; i <= 20; i += 1) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//     continue;
+//   }
+// }
+// for (let i = 0; i <= 10; i += 1) {
+//   console.log(`7 x ${i} = ${7 * i}`);
+// }
+// const n = 10;
+// for (let i = 0; i <= n; i += 1) {
+//   if (i >= n) {
+//     break;
+//   }
+//   console.log(i);
+// }
+// let i = 0;
+// while (i <= 20) {
+//   if (i % 3 === 0) {
+//     i += 1;
+//     continue;
+//   }
+//   console.log(i);
+//   i += 1;
+// }
